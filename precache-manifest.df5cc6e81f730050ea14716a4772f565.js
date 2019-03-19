706e8df16fa3cfddf6a51bf26e3eb2d021e85eb3
mariagaa993 <mariagaa993@gmail.com>
2019-03-19 09:25:53 -0400

    Updates

---
+++ precache-manifest.df5cc6e81f730050ea14716a4772f565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a1d8a90cb3c44363e9",
+    "url": "/myreact/static/js/runtime~main.b769e414.js"
+  },
+  {
+    "revision": "aa977e85f4145beaebfb",
+    "url": "/myreact/static/js/main.1432b764.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/myreact/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "aa977e85f4145beaebfb",
+    "url": "/myreact/static/css/main.2ce8dafc.chunk.css"
+  },
+  {
+    "revision": "dcbc7b9e2e646f4136b5672c1c90e0b5",
+    "url": "/myreact/index.html"
+  }
+];
